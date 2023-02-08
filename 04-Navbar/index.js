cc9df562916a61212ad9556ca1fb88f3572e6b35
atahhabibi <habibiatah@gmail.com>
2023-02-08 10:52:57 -0800

    completed the navbar

--- 04-Navbar/index.js
+++ 04-Navbar/index.js
@@ -6,12 +6,13 @@ const navbarDOM=getElement('.navbar');
 
 toggleBtn.addEventListener('click',()=>{
 
+    console.log(toggleBtn);
+
     const pagesHeight=PagesDOM.getBoundingClientRect().height;
     const nabarHeight=navbarDOM.getBoundingClientRect().height;
 
    if(pagesHeight>nabarHeight){
        navbarDOM.style.height=`${ pagesHeight + nabarHeight }px`;
-       toggleBtn.style.transform="rotate(360deg)"
    }else{
       navbarDOM.style.height=`64px`;
    }
