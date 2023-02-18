a7a6e720890b8a7be073c599d839036de47ba7bc
atah <habibiatah@gmail.com>
2023-02-18 01:22:22 -0800

    completed the dark mode project

--- 19-Dark-mode/index.js
+++ 19-Dark-mode/index.js
@@ -1,15 +1,33 @@
+
 import {getElement} from './Utils.js';
 import { articles } from './data.js';
 
+
+
 const ToggleBtnDOM=getElement('.react-btn')
-const newHeading=getElement('.')
+const aricleContainerDOM=getElement('.article-container');
 
 
 
 
 ToggleBtnDOM.addEventListener('click',()=>{
-  
-    document.body.classList.toggle('dark-mode')
+    document.body.classList.toggle('dark-mode');
 
-    
+})
+
+window.addEventListener('DOMContentLoaded',()=>{
+    let article=articles.map((article)=>{
+
+        const{id,title,date,length,snippet:info}=article;
+        
+        const newDate=moment(date).format('MMMM Do, YYYY')
+        return ` <article class="news">
+        <h3 class="news-heading">${title}</h3>
+        <span class="news-date"> ${newDate}, ${length} min read</span>
+        <p class="news-info">${info}</p>
+    </article>`
+    }).join(" ");
+
+
+    aricleContainerDOM.innerHTML=article;
 })
