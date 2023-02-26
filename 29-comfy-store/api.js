7aa043b4788c5daa323843abd86650f414c9354c
atah <habibiatah@gmail.com>
2023-02-26 11:26:01 -0800

    completed html part of comfy store

---
+++ 29-comfy-store/api.js
@@ -0,0 +1,7 @@
+export const singleProduct_API="https://course-api.com/javascript-store-single-product?id=";
+export const product_API="https://course-api.com/javascript-store-products";
+export const randomJoke_API="https://icanhazdadjoke.com/";
+export const github_API='https://api.github.com/users/john-smilga/followers?per_page=100';
+export const wiki_API="https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch="
+export const wiki_Page_url = 'http://en.wikipedia.org/?curid=';
+
