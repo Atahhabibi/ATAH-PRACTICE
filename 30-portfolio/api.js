153a757f7e78c6128c6758d6d5c0237e72b8329b
atah <habibiatah@gmail.com>
2023-03-02 07:52:54 -0800

    protofolio project in progress

---
+++ 30-portfolio/api.js
@@ -0,0 +1,4 @@
+export const singleProduct_API="https://course-api.com/javascript-store-single-product?id=";
+export const product_API="https://course-api.com/javascript-store-products";
+export const randomJoke_API="https://icanhazdadjoke.com/";
+
---
+++ 30-portfolio/index.js
@@ -0,0 +1,1 @@
+import {getElement} from './Utils.js';
