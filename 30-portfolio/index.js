2cb86d960a1dc1e7cf244ee6e54f1140da7dd56a
atah <habibiatah@gmail.com>
2023-03-03 08:03:19 -0800

    project in progress

--- 30-portfolio/index.js
+++ 30-portfolio/index.js
@@ -3,10 +3,14 @@ import {getElement} from './Utils.js';
 
 const toggleBtn=getElement('.toggle-btn');
 const linksContainerDOM=getElement('.page-links');
+const closeBtnDOM=getElement('.close-btn');
 
 toggleBtn.addEventListener('click',()=>{
+    linksContainerDOM.classList.add('show-links');
+})
 
-    linksContainerDOM.classList.toggle('show-links');
+closeBtnDOM.addEventListener('click',()=>{
+    linksContainerDOM.classList.remove('show-links')
 })
 
 
