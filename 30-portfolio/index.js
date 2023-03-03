39e4e860ab02cc0bb3d17298dfe7cdcde7aab807
atahhabibi <habibiatah@gmail.com>
2023-03-02 19:01:25 -0800

    working on portfoolio

--- 30-portfolio/index.js
+++ 30-portfolio/index.js
@@ -1,1 +1,12 @@
 import {getElement} from './Utils.js';
+
+
+const toggleBtn=getElement('.toggle-btn');
+const linksContainerDOM=getElement('.page-links');
+
+toggleBtn.addEventListener('click',()=>{
+
+    linksContainerDOM.classList.toggle('show-links');
+})
+
+
